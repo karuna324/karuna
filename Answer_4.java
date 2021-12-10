5859a0d506506ea42c154a4ae17a753cdc6e6fae
karuna324 <85303669+karuna324@users.noreply.github.com>
2021-12-10 07:44:41 -0800

    Add files via upload

---
+++ Answer_4.java
@@ -0,0 +1,23 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class Answer_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int n = sc.nextInt();
+        System.out.println("the integer are: ");
+        int sum=0 ;
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        System.out.println("the sum of number is");
+        for (int i = 1; i < n; i++) {
+            sum+= i;
+
+        }
+        System.out.println(sum);
+
+    }
+}
